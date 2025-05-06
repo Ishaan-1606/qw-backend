f350ab4f5bdd390abe13dca8b849b2f40ae16dc2
Ishaan Sharma <150211401+Ishaan-1606@users.noreply.github.com>
2025-05-06 14:43:08 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,23 @@
-import express from 'express'
-import {config} from "dotenv"
-import morgan from "morgan"
+import express from 'express';
+import { config } from 'dotenv';
+import morgan from 'morgan';
 import appRouter from './routes/index.js';
 import cookieParser from 'cookie-parser';
-import cors from 'cors'
-config();
-const app=express();
+import cors from 'cors';
 
-//check during deployment
-app.use(cors({origin:"http://localhost:5173",credentials:true}));
+config();
+
+const app = express();
+
+// Use CORS with frontend URL from environment
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
+// Development logging
+app.use(morgan('dev'));
 
-//remove during production
-app.use(morgan("dev"));
-
-app.use("/api/v1",appRouter);
+// Main route
+app.use('/api/v1', appRouter);
 
 export default app;
