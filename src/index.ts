7fe306f4837d0de7f0879469d091c484e5952e6c
Ishaan Sharma <150211401+Ishaan-1606@users.noreply.github.com>
2025-05-06 14:43:21 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import app from "./app.js"
-import { connectToDatabase } from "./db/connection.js"
+import app from './app.js';
+import { connectToDatabase } from './db/connection.js';
 
-const PORT=process.env.PORT||5000
-connectToDatabase().then(()=>{
-  app.listen(PORT,()=>{
-    console.log("Server open and connected to the db")
+const PORT = process.env.PORT || 5000;
+
+connectToDatabase()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log('Server open and connected to the DB');
+    });
   })
-}).catch((err)=>console.log(err));
-
+  .catch((err) => console.log(err));
